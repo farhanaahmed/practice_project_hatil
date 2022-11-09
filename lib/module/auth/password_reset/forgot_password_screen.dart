87e24cb4f36ef13c6dbf25c180d72e433ad9c0e9
Farhana Ahmed <farhana.ahmed.fima@gmail.com>
2022-11-09 11:46:48 +0600

    complete forgot_password_screen

--- lib/module/auth/password_reset/forgot_password_screen.dart
+++ lib/module/auth/password_reset/forgot_password_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_project_hatil/module/auth/login/login_screen.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
@@ -8,11 +9,91 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        "Forgot Password",
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.close,
+            size: 32,
+            color: Colors.black87,
+          ),
+          onPressed: () {
+            Navigator.pop(context,
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+          // tooltip: MaterialLocalizations.of(context)
+          //   .openAppDrawerTooltip,
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        child: ListView(
+          children: [
+            Text(
+              "Forgot password",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Please enter your email address.You will receive a link to "
+                  "create a new password via email.",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Colors.grey),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Colors.grey),
+                ),
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                hintText: "Enter your email",
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            MaterialButton(
+              onPressed: () {
+              },
+              minWidth: double.infinity,
+              height: 50,
+              color: Colors.black,
+              child: Text(
+                "SEND",
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
